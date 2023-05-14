f66639da2e32be321bea600f37c5fbcea92e5a7c
Yutzer <girginyusufduha@gmail.com>
2023-05-14 22:09:36 +0300

    The code is being commited to be tested on Github.

--- robot.py
+++ robot.py
@@ -1,6 +1,7 @@
 
-last_twenty_data==[]
-def collect_data_of_sensor(single_data): #This function takes a single data from sensor and stores it.
+last_twenty_data = []
+def collect_data_of_sensor(): #This function takes a single data from sensor and stores it.
+    single_data = input("Data from sensor:")
     last_twenty_data.append(single_data)
 
 def reduce_up_to_20_data(): #This function provides up to twenty data counts.
@@ -15,17 +16,17 @@ def find_average_of_distance(): #This function calculates average of last 20 dat
     average==total/count
     return average
     
-def determine_robot_movement():
+def determine_robot_movement(): 
     while True:
-        collect_data_of_sensor(single_data)
+        collect_data_of_sensor()
 
         if len(last_twenty_data)>20:
             reduce_up_to_20_data()
             find_average_of_distance()
-            if average >= 10:
-                print("Continue.")
+            if find_average_of_distance() >= 10:
+                print ("Continue.")
             else:
-                print("Stop.")
+                print ("Stop.")
                 break
 
-
+determine_robot_movement()
