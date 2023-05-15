58048de40233b4043b19239a76941e4299e87d29
Yutzer <girginyusufduha@gmail.com>
2023-05-15 13:36:34 +0300

    Functions are arranged

--- robot.py
+++ robot.py
@@ -1,28 +1,38 @@
 
 last_twenty_data = [10]
 last_twenty_data.pop()
+"""
 def collect_data_of_sensor(): #This function takes a single data from sensor and stores it.
     single_data = input("Data from sensor:")
     last_twenty_data.append(single_data)
 
-def reduce_up_to_20_data(): #This function provides up to twenty data counts.
-    last_twenty_data.pop(0)
+def collocate_last_20_data(): #This function provides up to twenty data counts.
+    
+    while True:
+        collect_data_of_sensor()
+    
+        if len(last_twenty_data) > 20:
+            last_twenty_data.pop(0)
+            print("It's working.", len(last_twenty_data)
+            break
 
+
+"""
 def find_average_of_distance(): #This function calculates average of last 20 data.
-    total==0
-    count==0
+    total = 0
+    count =10 0
     for i in last_twenty_data:
         total+=i
-        count+1
-    average==total/count
+        count+=1
+    average = total/count
     return average
     
 def determine_robot_movement(): 
     while True:
         collect_data_of_sensor()
 
-        if len(last_twenty_data)>20:
-            reduce_up_to_20_data()
+        if len(last_twenty_data) > 20:
+            collocate_20_data()
             find_average_of_distance()
             if find_average_of_distance() >= 10:
                 print ("Continue.")
@@ -31,3 +41,5 @@ def determine_robot_movement():
                 break
 
 determine_robot_movement()
+"""
+collocate_last_20_data()
