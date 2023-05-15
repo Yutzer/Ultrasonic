3b1339efdeceb1e9ccc1fa3f9d07538a82d4428f
Yutzer <girginyusufduha@gmail.com>
2023-05-15 16:11:12 +0300

    A function's name changed, explanation lines were added and changed

--- robot.py
+++ robot.py
@@ -1,12 +1,11 @@
 
-last_twenty_data = [10]
-last_twenty_data.pop()
+last_twenty_data = [10] #This list is created to include 20 data that will contribute to determine robot's movement. It has just 10 (could be any integer instead of it). This value will be removed.
 
 def collect_data_of_sensor(): #This function takes a single data from sensor and stores it.
     single_data = input("Data from sensor:")
     last_twenty_data.append(int(single_data))
 
-def collocate_last_20_data(): #This function provides up to twenty data counts.
+def set_last_20_data(): #This function provides up to twenty data counts and organises them. It uses data collection function.
     
     while True:
         collect_data_of_sensor()
@@ -25,9 +24,9 @@ def find_average_of_distance(): #This function calculates average of last 20 dat
     average = total/count
     return average
     
-def determine_robot_movement(): 
+def determine_robot_movement(): #This function decides robot to continue to move or stop. It contains average and twenty datas' function.
     while True:
-        collocate_last_20_data()
+        set_last_20_data()
         find_average_of_distance()
         if find_average_of_distance() >= 10:
             print ("Continue.")
@@ -35,4 +34,4 @@ def determine_robot_movement():
             print ("Stop.")
             break
 
-determine_robot_movement()
+determine_robot_movement() #This code runs determination function.
